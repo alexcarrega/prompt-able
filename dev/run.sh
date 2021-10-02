0347e7f41f8f1f92bda665db57b29a2eee5f86eb
Alex Carrega <contact@alexcarrega.com>
2021-10-02 11:54:15 +0200

    Dev utils

--- dev/run.sh
+++ dev/run.sh
@@ -3,4 +3,4 @@
 # Copyright (c) 2020-2029 Alex Carrega <contact@alexcarrega.com>
 # author: Alex Carrega <contact@alexcarrega.com>
 
-vprof -c cmh main.py --output-file dev/profile.json
+vprof -c cmh src/main.py --output-file dev/profile.json
